16be1aafd1f24f06826c49faf1b8800d227325ad
Vatsan Raman <sraman@genetics.med.harvard.edu>
2012-05-21 12:56:22 -0400

    Testing out TimeCourse and WellContent objects

--- run.py
+++ run.py
@@ -2,6 +2,8 @@
 
 from phil import *
 from Experiment import *
+from TimeCourse import *
+from WellContent import *
 
 def Help():
     print 'Script for testing objects'
@@ -11,9 +13,29 @@ def Help():
 if len(argv) < 2:
     print Help()
 
-inputfile = sys.argv[1]
+excelfile = sys.argv[1]
+wellcontent_file1 = sys.argv[2]
+wellcontent_file2 = sys.argv[3]
 
-expt1 = Experiment(inputfile)
+selection_gradient = WellContent(wellcontent_file1)
+inducer_gradien = WellContent(wellcontent_file2)
+
+expt1 = Experiment(excelfile)
 print expt1.total_timepoints()
 print expt1.total_timecourses()
-print expt1.extract_timecourse('A12')
+#print expt1.extract_timecourse('A1')
+
+#A1 = TimeCourse('A1',expt1.extract_timecourse('A1'))
+#A1.display()
+#print A1.mean()
+
+test_key_set = ['A1','A2','A3','A4']
+
+plate_data_object_list = []
+#for item in plate_to_excel.keys(): #turn on later
+for item in test_key_set:
+    tmp = TimeCourse(item,expt1.extract_timecourse(item))
+    plate_data_object_list.append(tmp)
+
+for item in plate_data_object_list:
+    print item, item.name, item.data()
