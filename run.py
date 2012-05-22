244b4e5dc70c2f3cb014c41c5a0226d01067d025
Vatsan Raman <sraman@genetics.med.harvard.edu>
2012-05-21 21:54:08 -0400

    Some clean. Moved some code the Classes

--- run.py
+++ run.py
@@ -19,18 +19,15 @@ wellcontent_file1 = sys.argv[2]
 wellcontent_file2 = sys.argv[3]
 outputfile = sys.argv[4]
 
-def normList(L, normalizeTo=1):
-    vMax = max(L)
-    return [ float('%1.4f'%(x/(vMax*1.0)*normalizeTo)) for x in L]
 
 selection_gradient = WellContent(wellcontent_file1)
 inducer_gradient = WellContent(wellcontent_file2)
-print selection_gradient.conc_at('A1')
-print inducer_gradient.conc_at('A1')
+#print selection_gradient.conc_at('A1')
+#print inducer_gradient.conc_at('A1')
 
 expt1 = Experiment(excelfile)
-print expt1.total_timepoints()
-print expt1.total_timecourses()
+#print expt1.total_timepoints()
+#print expt1.total_timecourses()
 #print expt1.extract_timecourse('A1')
 
 #A1 = TimeCourse('A1',expt1.extract_timecourse('A1'))
@@ -50,7 +47,6 @@ for item in test_key_set:
 selection = []
 inducer = []
 OD = []
-normalized_OD = []
 
 for item in plate_data_object_list:
     print item.name, item.mean(),selection_gradient.conc_at(item.name),inducer_gradient.conc_at(item.name)
@@ -58,17 +54,8 @@ for item in plate_data_object_list:
     inducer.append(inducer_gradient.conc_at(item.name))
     OD.append(item.mean())
 
-normalized_OD = normList(OD) #NOTE: we are plotting OD normalized to 1
-heat_map = Plotter('junk',selection,inducer,normalized_OD)
+#normalized_OD = normList(OD) #NOTE: we are plotting OD normalized to 1
+heat_map = Plotter('junk',selection,inducer,OD)
 heat_map.plot()
 
-exit()
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.set_xscale('log')
-ax.set_yscale('log')
-ax.scatter(selection,inducer,c=normalized_OD,cmap = plt.jet(), s=670, lw = 0.1, marker = 's')
-bar = fig.colorbar(ax.collections[0])
-plt.show()
-fig.savefig('%s.png'%outputfile)
