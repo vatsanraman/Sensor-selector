0a3356623db3980dc0b4a954b1096fbf1f2a0355
Vatsan Raman <sraman@genetics.med.harvard.edu>
2012-06-12 23:22:48 -0400

    Methods to make a local copy of the excel file in txt format for faster access

--- Experiment.py
+++ Experiment.py
@@ -10,7 +10,16 @@ class Experiment:
         
     def __init__(self, name):
         self.name = name
+        self.preextracted_file_exists = False
         print "Initializing ",self.name
+        self.preextracted_file = '.'+self.name[:-5]+'.txt'
+        if not os.path.isfile(self.preextracted_file):
+            print "Pre extracted file does not exist"
+            self.preextract_file()
+        else:
+            print "Read from pre-extracted file"
+            self.__open_preextracted_file()
+            self.preextracted_file_exists = True
     
     def __open_worksheet(self):
         wb = load_workbook(self.name)
@@ -24,6 +33,9 @@ class Experiment:
         return self.__open_worksheet().get_highest_column()
     
     def extract_timecourse(self,well):
+        if self.preextracted_file_exists:
+            return self.__preextracted_file_extract_timecourse(well)
+            
         ws = self.__open_worksheet()
         timecourse = []
         timepoints = self.total_timepoints()
@@ -41,7 +53,36 @@ class Experiment:
     def extract_all_timecourses(self):
         test_key_set = ['A1', 'A2', 'A3', 'A4']### REPLACE WITH FULL SET
         all_timecourses = {}
-        for item in test_key_set:
+        for item in plate_to_excel:
             if not all_timecourses.has_key(item):
                 all_timecourses[item] = TimeCourse(item, self.extract_timecourse(item))
         self.all_timecourses = all_timecourses
+
+    def preextract_file(self):
+        self.extract_all_timecourses()
+        data = open(self.preextracted_file,'w')
+        for keys in self.all_timecourses:
+            data.write('%s '%keys)
+            for item in self.all_timecourses[keys].data():
+                data.write('%1.4f '%item)
+            data.write('\n')
+
+    def __open_preextracted_file(self):
+        test_key_set = ['A1', 'A2', 'A3', 'A4']
+        all_timecourses = {}
+        data = open(self.preextracted_file, 'r')
+        line = data.readline()
+        while line:
+            l = string.split(line)
+            tmp = []
+            tmp = [float(i) for i in l[1:]]
+            if not all_timecourses.has_key(l[0]):
+                all_timecourses[l[0]] = TimeCourse(l[0],tmp)
+            line = data.readline()
+        self.all_timecourses = all_timecourses
+        
+    
+    def __preextracted_file_extract_timecourse(self, well):
+        return self.all_timecourses[well].data()
+    
+
